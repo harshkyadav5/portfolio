1b6ec8257a11e8a89d55937a03c0eef6074d5dc4
harshkyadav5 <harshkyadav5@gmail.com>
2025-08-27 23:49:26 +0530

    Make particle respond to scroll

--- src/components/ParticleAnimation.jsx
+++ src/components/ParticleAnimation.jsx
@@ -1,66 +1,96 @@
 import React, { useEffect, useRef } from 'react';
 import { gsap } from 'gsap';
-import { useGSAP } from 'gsap;'
+import { ScrollTrigger } from 'gsap/all';
 import './ParticleAnimation.css';
 
+gsap.registerPlugin(ScrollTrigger);
+
 const particlesData = [
-    { selector: '.particle-1', xRange: [-15, 21], yRange: [-12, 20], rotation: 20 },
-    { selector: '.particle-2', xRange: [-15, 21], yRange: [-20, 24], rotation: -26 },
-    { selector: '.particle-3', xRange: [-4, 8], yRange: [-4, 8], rotation: 17 },
-    { selector: '.particle-4', xRange: [-5, 6], yRange: [-3, 6], rotation: 20 },
-    { selector: '.particle-5', xRange: [-4, 6], yRange: [-6, 4], rotation: 35 },
-    { selector: '.particle-6', xRange: [-7, 4], yRange: [-9, 4], rotation: 23 },
-    { selector: '.particle-7', xRange: [-9, 6], yRange: [-6, 4], rotation: -26 },
-    { selector: '.particle-8', xRange: [-8, 4], yRange: [-4, 9], rotation: -49 },
+  { selector: '.particle-1', xRange: [-15, 21], yRange: [-12, 20], rotation: 20 },
+  { selector: '.particle-2', xRange: [-15, 21], yRange: [-20, 24], rotation: -26 },
+  { selector: '.particle-3', xRange: [-4, 8], yRange: [-4, 8], rotation: 17 },
+  { selector: '.particle-4', xRange: [-5, 6], yRange: [-3, 6], rotation: 20 },
+  { selector: '.particle-5', xRange: [-4, 6], yRange: [-6, 4], rotation: 35 },
+  { selector: '.particle-6', xRange: [-7, 4], yRange: [-9, 4], rotation: 23 },
+  { selector: '.particle-7', xRange: [-9, 6], yRange: [-6, 4], rotation: -26 },
+  { selector: '.particle-8', xRange: [-8, 4], yRange: [-4, 9], rotation: -49 },
 ];
 
 export default function ParticleAnimationGSAP() {
-    const wrapperRef = useRef(null);
+  const wrapperRef = useRef(null);
 
-    useEffect(() => {
-        const wrapper = wrapperRef.current;
-        if (!wrapper) return;
+  useEffect(() => {
+    const wrapper = wrapperRef.current;
+    if (!wrapper) return;
 
-        const particles = gsap.utils.toArray('.particle', wrapper);
-        
-        gsap.set(wrapper, { scale: 1.5, opacity: 0 });
+    const particles = gsap.utils.toArray('.particle', wrapper);
 
-        const loadTimeline = gsap.timeline();
-        loadTimeline.to(wrapper, { duration: 0.5, opacity: 1, scale: 1, ease: 'power2.out' });
+    gsap.set(wrapper, { scale: 1.5, opacity: 0 });
+    gsap.to(wrapper, {
+      duration: 1,
+      opacity: 1,
+      scale: 1,
+      ease: 'power2.out'
+    });
 
-        const handleMouseMove = (e) => {
-            const mouseX = e.clientX;
-            const mouseY = e.clientY;
+    const handleMouseMove = (e) => {
+      const mouseX = e.clientX;
+      const mouseY = e.clientY;
 
-            particles.forEach((particle, index) => {
-                const data = particlesData[index];
-                
-                const xOffset = gsap.utils.mapRange(0, window.innerWidth, data.xRange[0], data.xRange[1], mouseX);
-                const yOffset = gsap.utils.mapRange(0, window.innerHeight, data.yRange[0], data.yRange[1], mouseY);
+      particles.forEach((particle, index) => {
+        const data = particlesData[index];
 
-                gsap.to(particle, {
-                    x: xOffset,
-                    y: yOffset,
-                    rotate: data.rotation,
-                    duration: 1.5,
-                    ease: 'power2.out'
-                });
-            });
-        };
+        const xOffset = gsap.utils.mapRange(
+          0,
+          window.innerWidth,
+          data.xRange[0],
+          data.xRange[1],
+          mouseX
+        );
+        const yOffset = gsap.utils.mapRange(
+          0,
+          window.innerHeight,
+          data.yRange[0],
+          data.yRange[1],
+          mouseY
+        );
 
-        window.addEventListener('mousemove', handleMouseMove);
-        
-        return () => {
-            window.removeEventListener('mousemove', handleMouseMove);
-        };
+        gsap.to(particle, {
+          x: xOffset,
+          y: yOffset,
+          rotate: data.rotation,
+          duration: 1.5,
+          ease: 'power2.out'
+        });
+      });
+    };
 
-    }, []);
+    gsap.to('.particle-wrapper', {
+      ease: 'power1.inOut',
+      scrollTrigger: {
+        trigger: '.particle-wrapper',
+        start: 'top top',
+        end: 'bottom top',
+        scrub: true,
+        onUpdate: (self) => {
+          const newScale = 1 + self.progress;
+          gsap.set('.particle-wrapper', { scale: newScale });
+        },
+      },
+    });
 
-    return (
-        <div ref={wrapperRef} className="particle-wrapper">
-            {particlesData.map((data, index) => (
-                <div key={index} className={`particle ${data.selector.substring(1)}`}></div>
-            ))}
-        </div>
-    );
-};
+    window.addEventListener('mousemove', handleMouseMove);
+
+    return () => {
+      window.removeEventListener('mousemove', handleMouseMove);
+    };
+  }, []);
+
+  return (
+    <div ref={wrapperRef} className="particle-wrapper">
+      {particlesData.map((data, index) => (
+        <div key={index} className={`particle ${data.selector.substring(1)}`}></div>
+      ))}
+    </div>
+  );
+}
