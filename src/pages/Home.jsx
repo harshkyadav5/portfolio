8a3d64d24dc3f2ef3657179e490d7219b98241c6
harshkyadav5 <harshkyadav5@gmail.com>
2025-09-01 16:10:17 +0530

    Replace demo project card with real

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import Hero from "../components/Hero";
 import ProjectCard from "../components/ProjectCard";
-import snapIcon from "../assets/SnapchatIcon.png";
-import snap from "../assets/Snapchat.png";
+import tabsIcon from "../assets/tabsIcon.png";
+import tabs from "../assets/tabs.png";
 import Footer from "../components/Footer";
 
 export default function Home() {
@@ -11,24 +11,16 @@ export default function Home() {
 
         <div className="flex flex-col justify-center items-center gap-25 mb-25 antialiased load">
           <ProjectCard
-            icon={snapIcon}
-            title="Simple Snapchat"
-            subtitle="Snapchat"
-            year="2024"
-            description="Pitched the original prototype and led design for a simpler app that brings Snapchatters closer to their friends."
-            image={snap}
-            onButtonClick={() => alert("Read more clicked")}
-          />
-          <ProjectCard
-            icon={snapIcon}
-            title="Simple Snapchat"
-            subtitle="Snapchat"
-            year="2024"
-            description="Pitched the original prototype and led design for a simpler app that brings Snapchatters closer to their friends."
-            image={snap}
-            onButtonClick={() => alert("Read more clicked")}
+            icon={tabsIcon}
+            title="Tabs"
+            subtitle="Web | Extension"
+            year="2025"
+            description="Designed a unified workspace that brings together notes, bookmarks, and more in one seamless web and extension experience"
+            url="https://github.com/harshkyadav5/tabs"
+            image={tabs}
           />
         </div>
+
         <Footer />
     </>
   );
